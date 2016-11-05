645fc0b4596b3c5ae24d6036973a4679494310e5
Johnny J <Johnny J>
2016-11-05 11:25:44 -0400

    comments added

--- Perceptron.py
+++ Perceptron.py
@@ -8,27 +8,34 @@ Created on Tue Oct 25 20:50:38 2016
 import numpy as np
 
 class Perceptron(object):
+    #eta - learning rate
+    #n_iter - the number of epochs or passes over the training data. 
     def __init__(self, eta=0.01, n_iter=10):
         self.eta = eta
         self.n_iter = n_iter
     
-    def fit(self, X, y):
+    def fit(self, X, y):  #Perceptron Model 
         self .w_ = np.zeros(1+X.shape[1])
         self.errors_ = []
 
         for _ in range(self.n_iter):
             errors = 0
             for xi, target in zip(X,y):
-                update = self.eta * (target - self.predict(xi))
-                self.w_[1:] += update * xi
+                update = self.eta * (target - self.predict(xi))  #compute the predicted output value.
+                self.w_[1:] += update * xi  #Update the weights.  
                 self.w_[0] += update
                 errors += int(update != 0.0)
             self.errors_.append(errors)
         return self
         
-    def net_input(self,X):
+    #For 2-D arrays it is equivalent to matrix multiplication, and for 1-D arrays to inner product of vectors
+    # so in this case it is doing N  1X2 * 1X2 as an inner product to get so x1*w1 + x2*w2 as the result.
+    # so we get a 1 row by 300 column matrix returned....ugh
+    def net_input(self,X):  # Calculating Z which is X dot transpose(W)
+        #print (np.dot(X, self.w_[1:]))
         return np.dot(X, self.w_[1:]) + self.w_[0]
 
-    def predict(self, X):
-        return np.where(self.net_input(X) >= 0.0, 1 -1)
+    def predict(self, X):  #activation function or the unit step function
+        #print (self.w_[1:])
+        return np.where(self.net_input(X) >= 0.0, 1, -1)
         
--- tutorial.py
+++ tutorial.py
@@ -9,31 +9,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
 import numpy as np
-
-class Perceptron(object):
-    def __init__(self, eta=0.01, n_iter=10):
-        self.eta = eta
-        self.n_iter = n_iter
-    
-    def fit(self, X, y):
-        self .w_ = np.zeros(1+X.shape[1])
-        self.errors_ = []
-
-        for _ in range(self.n_iter):
-            errors = 0
-            for xi, target in zip(X,y):
-                update = self.eta * (target - self.predict(xi))
-                self.w_[1:] += update * xi
-                self.w_[0] += update
-                errors += int(update != 0.0)
-            self.errors_.append(errors)
-        return self
-        
-    def net_input(self,X):
-        return np.dot(X, self.w_[1:]) + self.w_[0]
-
-    def predict(self, X):
-        return np.where(self.net_input(X) >= 0.0, 1, -1)
+from Perceptron import Perceptron
 
 #columns are Sepal Length, Sepal Width, Petal Length, Petal Width, Class Label
 #First 50 is Iris-setosa
@@ -62,6 +38,7 @@ plt.legend(loc='upper left')
 plt.show()
 
 
+#Create the model.  This will determine the weights to appropriately class realworld input data.
 ppn = Perceptron(eta=0.1, n_iter = 10)
 ppn.fit(X,y)
 plt.plot(range(1, len(ppn.errors_) + 1), ppn.errors_, marker='o')
@@ -74,12 +51,26 @@ def plot_decision_regions(X, y, classifier, resolution = 0.02):
     colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
     cmap = ListedColormap(colors[:len(np.unique(y))])
     
+    #Getting the min and max of the two features so we can plot them nicely.
     x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
     x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    
+    #np.arange(x1_min, x1_max, resolution)) - length 235
+    #np.arange(x2_min, x2_max, resolution)) - length 305
+    #np.arange Return evenly spaced values within a given interval.
+    # In the 2-D case with inputs of length M and N, the outputs are of shape (N, M) for ‘xy’ indexing
     xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
                            np.arange(x2_min, x2_max, resolution))
+    
+    #print (xx1, "\n" , xx2)
+    #Creates a 2-D array of sepal length vs. petal length
+    #Data is evenly distributed between min/max of their respective feature.
+    #print (np.array([xx1.ravel(), xx2.ravel()]).T)  
+    
     Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    #print (Z)
     Z = Z.reshape(xx1.shape)
+    #print (Z)
     plt.contourf(xx1, xx2, Z, alpha=0.4, cmap=cmap)
     plt.xlim(xx1.min(), xx1.max())
     plt.ylim(xx2.min(), xx2.max())
